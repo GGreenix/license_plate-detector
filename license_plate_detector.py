19ad7d31b7df3ed89b6f918a04de51a1265195bb
Tal Ben-Amram <talikoob@gmail.com>
2021-03-18 11:21:17 +0200

    final final commit

--- license_plate_detector.py
+++ license_plate_detector.py
@@ -53,6 +53,7 @@ def get_minimum_bounding_box(contur):
     box = np.int0(box)
     x,y,w,h = cv2.boundingRect(contur)
     return x,y,w,h
+    
 
 def extract_license_from_image(license_plate_image,thresh_image,parameters):  
     x,y,w,h = parameters
@@ -67,6 +68,14 @@ def inRange(range,value_to_compare):
 
 
 def is_pixel_black_enough(pixel):
+    '''
+    input (pixel value)
+
+    this function is used for checking if given pixel HSV
+    values are between constant range
+
+    output (if H_value,S_value,V_value are between the constant range of (0,0,20)-(50,255,140))
+    '''
     lower_black = (0,0,20)
     higher_black = (50,255,140)
     p_h,p_s,p_v = pixel[0],pixel[1],pixel[2]
@@ -76,7 +85,20 @@ def is_pixel_black_enough(pixel):
 
 
 def check_for_black_pixels_in_roi(image_roi,thresh_roi):
-    sum_of_black_pixels = 0 #UwU
+    '''
+    input (region of image of most likely a license plate, region of image in the threshold image)
+    
+    this function is comparing side by side
+    each and every pixel of the flattened region
+    of image by, then it calculates the ratio
+    between the aproved black pixels and the rest of the 
+    thresholded pixels by summerizing all of the white pixels
+    within the thresholded image
+
+    return (if ratio of black pixels to the rest of the thresholded pixels is between 1-10)
+
+    '''
+    sum_of_black_pixels = 0 
     
     image_hsv_roi = cv2.cvtColor(image_roi,cv2.COLOR_BGR2HSV)
     new_shape = image_roi.shape[0]*image_roi.shape[1]
@@ -87,17 +109,15 @@ def check_for_black_pixels_in_roi(image_roi,thresh_roi):
         if is_pixel_black_enough(i):
             sum_of_black_pixels += 1
     
+    return inRange((1,10),sum_of_thresholded_pixels/sum_of_black_pixels)
     
-    return 1 < sum_of_thresholded_pixels/sum_of_black_pixels and sum_of_thresholded_pixels/sum_of_black_pixels < 10
             
         
     
 
 def main():
-    image = cv2.imread('more cars.jpg')
-    #Import only if not previously imported
+    image = cv2.imread('9.jfif')
     
-         #(flag = 0 or 1 or -1)
     thresholded_car_image = get_threshold(image)
 
     conturs,_ = cv2.findContours(thresholded_car_image,cv2.RETR_EXTERNAL,cv2.RETR_CCOMP)
@@ -105,15 +125,15 @@ def main():
     
     
     license_plates = [get_minimum_bounding_box(contur) for contur in conturs if First_license_plate_filter(contur)]
-    print(license_plates)
+    
     
     for params in license_plates:
         
-        x,y,w,h = params
+        x,y,w,h = params #parameters of the bounding rectangle
         
         
         if(check_for_black_pixels_in_roi(extract_license_from_image(image,thresholded_car_image,(x,y,w,h)),thresholded_car_image[y:y+h,x:x+w])):
-            image[y:y+h,x:x+w] = [0,0,0]
+            image[y:y+h,x:x+w] = [0,0,0] #blacks out all pixels within the region of the bounding rectangle
    
        
 
